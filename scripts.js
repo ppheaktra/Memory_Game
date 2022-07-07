c2fce62e730dd7d3002e754bf10ac42a31efa312
ppheaktra <yimchanpheaktra9@gmail.com>
2022-07-07 12:23:52 -0700

    get timer and button to work

--- scripts.js
+++ scripts.js
@@ -1,38 +1,63 @@
 // Global variables
 const timer = document.querySelector('#game-timer')
-const restartBtn = document.querySelector('.start-Again')
+const restartBtn = document.querySelector('.startAgain')
 const squares = document.querySelectorAll('.square')
+
+const result = document.querySelector('#result')
+const message = document.querySelector('#message')
 
 let firstCard , secondCard;
 let selectCard = false;
 let cardsMatched = 0;
 
-//let time = 0;
 let minutes = 0;
-let seconds = 40;
+let seconds = 05;
 const minDiv = document.querySelector('#minutes')
 minDiv.innerText = minutes;
 const secDiv = document.querySelector('#seconds')
 secDiv.innerText = seconds;
-const timeCount = () => {
-    seconds -= 1;
-    secDiv.innerText = ""
-    secDiv.innerText = seconds;
+// const timeCount = () => {
+//     seconds -= 1;
+//     setInterval(() =>{
+//         secDiv.innerText = "";
+//         secDiv.innerText = seconds;
+//     }, 1000);
+    // secDiv.innerText = "";
+    // secDiv.innerText = `${seconds}`;
+
+//show message if player doesn't make it on time
+let displayMessage = (losingMessage) =>{
+    message.innerText = losingMessage
+    selectCard = true;
 }
+const play = () =>{
+    //timeCount ()
+    const timer = setInterval(() =>{
+        seconds -= 1
+       
+        if (seconds <= 0){
+            clearInterval(timer)
+        }
+        
+        secDiv.innerText = "";
+        secDiv.innerText = seconds;
+    }, 1000);
+    // if (seconds < 0){
+    //     clearInterval(timer)
+    // }
 
 
-//     for(let i = 0; i < squares.length; i++) {
-//         squares[i].Math.floor(Math.random()*16) + 1;
-//         shuffleCard();
-//         squares.forEach((square) => {
-//             square.classList.remove('flip')
-//             square.addEventListener('click', flipCards)
-//         })
-// //        console.log(squares)
-//     }
+
+
+
+
+
+
+
+
 // function flip cards
 function flipCard(e){
-    setTimeout(timeCount, 1000)
+    // setTimeout(timeCount, 1000)
     let chosenCard = e.target;
     chosenCard.classList.add('flip');
 
@@ -65,17 +90,15 @@ function shuffleCard () {
     });
 }
 shuffleCard();
-//when both matched its 
+//check 2 cards when unmatched, it shakes and flip back
 function matchCards (img1, img2) {
     if (img1 === img2){
         cardsMatched++; //increment matching cards value by 1
-        if(cardsMatched == 8){
+        if(cardsMatched == 8){ // if 8 images all matched then its shuffled
             setTimeout(()=> {
                 return shuffleCard();
-
-            }, 1200);
-            
-        }
+        }, 1200);
+    }      
         firstCard.removeEventListener('click', flipCard)
         secondCard.removeEventListener('click', flipCard)
         firstCard = secondCard = '';
@@ -84,54 +107,57 @@ function matchCards (img1, img2) {
         setTimeout(() => {
             firstCard.classList.add('shake')
             secondCard.classList.add('shake')
-        }, 700);
+        }, 500);
         setTimeout(()=> {
             firstCard.classList.remove('shake', 'flip');
             secondCard.classList.remove('shake', 'flip');
             firstCard = secondCard = '';
             selectCard = false;
-        }, 1000);
+        }, 1200);
     }
 }
-// removeCards();
+// function checkCard() {
+//     if (firstCard.getAttribute('name') === secondCard.getAttribute('name')) {
+//         removeCards();
+//     } 
 
-// function removeCards() {
-//     firstCard.removeEventListener('click', flipCard);
-//     secondCard.removeEventListener('click', flipCard);
-//     setTimeout(function()  {
-//         firstCard.style.visibility = secondCard.style.visibility = 'hidden';
-//         resetGame();
-//     }, 500);
 // }
+
+// remove both cards if they're matched
+function removeCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    setTimeout(function()  {
+        firstCard.style.visibility = secondCard.style.visibility = 'hidden';
+    }, 500);
+}
+
+
+
 // function resetGame() {
 //     selectCard = false;
 //     firstCard = null;
 //     secondCard = null;
 // }
 
-function startGame () {
-    
-}
+// function startGame () {
+//     if(click==)
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // squares.forEach((firstCard, index) =>{
+    //     firstCard[index] = null;
+    // });
+    // squares.forEach((square) =>{
+    //      square.innerText = "";
+        // selectCard = false;
+        //let imgCard = square.querySelector('img')
+        //imgCard.src = `images/img-${imgArr[index]}.png`;  
 
 //event listener for each squares turnable when clicks
 squares.forEach(square => {
     square.addEventListener('click', flipCard);
 
-//square.classList.add('flip')
 })
-//event listener for button to start the game 
-restartBtn.addEventListener('click', startGame);
+
+}//event listener for button to start the game 
+restartBtn.addEventListener('click', play);
